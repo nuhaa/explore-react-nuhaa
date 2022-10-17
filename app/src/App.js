06d4f91b08eb8ec6b437fd57cd821d47f68fcf1f
Muhammad Ulin Nuha <nuha.um@gmail.com>
2022-10-17 18:14:19 +0700

    router

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from react-router-dom;
+
+function App() {
+  return
+    <></>
+  ;
+}
+
+export default App;
